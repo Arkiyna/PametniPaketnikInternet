6fe75547c120f5028a4f3c8da89fdd51af4c5c85
Miha <mihagrubelnik123@gmail.com>
2022-05-06 22:52:35 +0200

    Poprava napake pri prijavi in preverjanje, če uporabniško ime že obstaja pri registraciji.

--- models/User.php
+++ models/User.php
@@ -13,26 +13,39 @@ class User
         $this->password = $password;
         $this->email = $email;
     }
-    public function dodaj(){
+    function username_exists($username){
         $db = Db::getInstance();
+
+        $query = "SELECT * FROM User WHERE username='$username'";
+        $res = $db->query($query);
+        return mysqli_num_rows($res) > 0;
+    }
+    public function dodaj(){
         $username = $this->username;
         $password = sha1($this->password);
         $email = $this->email;
-
-        mysqli_query($db, "INSERT INTO User VALUES (NULL, '$username', '$password', '$email')");
-
-        if(mysqli_error($db)) {
-            var_dump($db);
-            exit();
+        if($this->username_exists($username)){
+            echo json_encode("Zaseden username");
         }
+        else {
+            $db = Db::getInstance();
 
-        $this->id=mysqli_insert_id($db);
+
+            mysqli_query($db, "INSERT INTO User VALUES (NULL, '$username', '$password', '$email')");
+
+            if (mysqli_error($db)) {
+                var_dump($db);
+                exit();
+            }
+
+            $this->id = mysqli_insert_id($db);
+        }
     }
     public static function login($username,$password) {
-
+        $password=sha1($password);
         $db = Db::getInstance();
 
-        if($result = mysqli_query($db, "SELECT * FROM User WHERE username='test' AND password='test'")) {
+        if($result = mysqli_query($db, "SELECT * FROM User WHERE username='$username' AND password='$password'")) {
             while ($row = $result->fetch_assoc()) {
                 $user = new User($row["username"], $row["password"], $row["email"]);
                 return $user;
